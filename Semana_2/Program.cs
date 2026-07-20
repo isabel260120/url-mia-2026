2dc99ed78936056cf2d5e32ac98b35d836c457ca
Rosario Palma <ripalmao@coreo.url.edu.gt>
2026-07-20 17:39:23 -0600

    laboratorio semana 2

---
+++ Semana_2/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.Common;
+using System.Diagnostics.Contracts;
+using System.Text.Json;
+
+
+
+class Program
+{
+    static void Main(string[]args)
+    {
+        string rutaCsv="estudiantes.csv";
+        string rutaJson="estudiantes.json";
+
+        string[]lineas=File.ReadAllLines(rutaCsv);
+        List<Estudiante>estudiantes=new List<Estudiante>();
+        for(int i = 1; i < lineas.Length; i++)
+        {
+            string linea=lineas[i];
+            if (string.IsNullOrWhiteSpace(linea))
+            {
+                continue;
+            }
+            string[]datos=linea.Split(',');
+            
+            Estudiante estudiante=new Estudiante
+            {
+                Id=int.Parse(datos[0]),
+                Nombre=datos[1],
+                Carrera=datos[2]
+            };
+            estudiantes.Add(estudiante);
+
+        }
+        foreach(Estudiante e in estudiantes)
+        {
+            Console.WriteLine($"{e.Id}-{e.Nombre}-{e.Carrera}");
+        }
+        var opciones=new JsonSerializerOptions{WriteIndented=true};
+        string json=JsonSerializer.Serialize(estudiantes,opciones);
+
+        File.WriteAllText(rutaJson,json);
+        Console.WriteLine("Archivo estudiantes.json creado correctamente");
+    }
+}
